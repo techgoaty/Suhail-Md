a3f3d6ffc80d39e63612babcc6bfc8aa0e5608e0
techgoaty <153655883+techgoaty@users.noreply.github.com>
2024-03-07 21:01:58 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //═══════[Required Variables]════════\\
 global.audio= "" ;  
-global.video= "" ;
+global.video= "https://i.imgur.com/YEmBO8d.mp4" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
 global.email ="samsamsun789@gmail.com"
@@ -19,15 +19,15 @@ global.DATABASE_URI = process.env.DATABASE_URL || ""
 
 global.timezone= process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
 global.github=process.env.GITHUB|| "https://github.com/SuhailTechInfo/Suhail-Md";
-global.gurl  =process.env.GURL  || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643";
-global.website=process.env.GURL || "https://whatsapp.com/channel/0029Va9thusJP20yWxQ6N643" ; 
+global.gurl  =process.env.GURL  || "https://chat.whatsapp.com/B6hifgu7KbF0Y6hRg0mJAp";
+global.website=process.env.GURL || "https://chat.whatsapp.com/B6hifgu7KbF0Y6hRg0mJAp" ; 
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/d5b1c3544fedc23e11a06.jpg" ; // SET LOGO FOR IMAGE 
 
 
 
-global.devs = "923184474176" // Developer Contact
-global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "null";
-global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "923184474176";
+global.devs = "2349169580302" // Developer Contact
+global.sudo = process.env.SUDO ? process.env.SUDO.replace(/[\s+]/g, '') : "2348118820641";
+global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]/g, '') : "2349169580302";
 
 
 
@@ -61,7 +61,7 @@ global.api_smd = "https://api-smd-1.vercel.app"
 global.scan = "https://suhail-md-vtsf.onrender.com/";
 
 
-global.SESSION_ID = process.env.SESSION_ID ||  "" ;
+global.SESSION_ID = process.env.SESSION_ID ||  "SUHAIL_19_54_03_07_ewogICJjcmVkcy5qc29uIjogIntcIm5vaXNlS2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiV09CclBpekpiNTZlVlN5WjZIL0JiMU1hQXVyZEpKSzFOYXdiS0tDOW9GMD1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIjZFUGQ2N0xpZFlNMXNKeHV3T2NGQ2swTStCaGM5UU5MdFg1MEsxSWpVblU9XCJ9fSxcInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyXCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiQUFIZ0hnamxjS3NKdHRMQlBQYy9HV200a2FWekgzb0VTSnZGcU5kLytYUT1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcImRWTzd4bXBNUVVJa2NtT21ueVdxUGhjT1pVWFBuTTA2ZWlDVHhYVUlzbmc9XCJ9fSxcInNpZ25lZElkZW50aXR5S2V5XCI6e1wicHJpdmF0ZVwiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwic0g1NUNVaFkvNExFRFBFK3BkbXFmejU4TllpMmNhZVFPL09razZ3bSsxND1cIn0sXCJwdWJsaWNcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcInJvdGhPZlNFdnBsMk1idk05Y1F1c1EyWkd2Ynk1a2pyUXB0cUppUFZ5QVk9XCJ9fSxcInNpZ25lZFByZUtleVwiOntcImtleVBhaXJcIjp7XCJwcml2YXRlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJTSHA1RHdWc0xhTVdWWXZuUjA4ak5Ed2NvNlFmSnd2RW02cEc0RjgrcFZBPVwifSxcInB1YmxpY1wiOntcInR5cGVcIjpcIkJ1ZmZlclwiLFwiZGF0YVwiOlwiRmpsK0NFRE4rTEdiTWc3Qk04VmdvS2hNRm1aazY5K3FRaFJVcHZPVVFGaz1cIn19LFwic2lnbmF0dXJlXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJOaXlaWTVWd3FVNG5WakF4QURNVTh1SU0rQlBNWmFCbi9zUVNORFZsT1Q0d2xwNlVneEE2NUtlYW9JOWZwTmt0VzhSUHVoQ3Ewd2tpWFBMU2lpQ0hEQT09XCJ9LFwia2V5SWRcIjoxfSxcInJlZ2lzdHJhdGlvbklkXCI6ODAsXCJhZHZTZWNyZXRLZXlcIjpcIk0yS09najJ2aGdubWlCZmI2d1dLZHNvTHMxZnd2VW5xL3NaMTVieXZyM2M9XCIsXCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXNcIjpbXSxcIm5leHRQcmVLZXlJZFwiOjYxLFwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWRcIjo2MSxcImFjY291bnRTeW5jQ291bnRlclwiOjAsXCJhY2NvdW50U2V0dGluZ3NcIjp7XCJ1bmFyY2hpdmVDaGF0c1wiOmZhbHNlfSxcImRldmljZUlkXCI6XCItTE5rYXN0eVIyeXFVT1E0c3ItTjh3XCIsXCJwaG9uZUlkXCI6XCI0OWZlMjMwOC0wYmE2LTRiY2ItOGQ1Yi1kODVmMGU3NTQ2MWVcIixcImlkZW50aXR5SWRcIjp7XCJ0eXBlXCI6XCJCdWZmZXJcIixcImRhdGFcIjpcIjlsQklLVzNBQk9PYmNCTVRBRDhWZEtOK3pVTT1cIn0sXCJyZWdpc3RlcmVkXCI6dHJ1ZSxcImJhY2t1cFRva2VuXCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJkdEV4cDZZS3JqSENPdkg1THpxc2pRMFFOK0E9XCJ9LFwicmVnaXN0cmF0aW9uXCI6e30sXCJwYWlyaW5nQ29kZVwiOlwiNENLMUxDS1NcIixcIm1lXCI6e1wiaWRcIjpcIjIzNDkxNjk1ODAzMDI6OUBzLndoYXRzYXBwLm5ldFwiLFwibmFtZVwiOlwi1aLVtGNrINW+1oXVtFwiLFwibGlkXCI6XCI5NjgyMTkwMDczODY5MDo5QGxpZFwifSxcImFjY291bnRcIjp7XCJkZXRhaWxzXCI6XCJDT08raVo0RUVQTzJxSzhHR0FFZ0FDZ0FcIixcImFjY291bnRTaWduYXR1cmVLZXlcIjpcImlJSCtKRWFFNWI0NFppZkFzekFQMnpNSzdCSDZNNktFdGdabmUxT29nams9XCIsXCJhY2NvdW50U2lnbmF0dXJlXCI6XCJlQXBNeDFNZzJ6MmpTemRtQndRNzliUzRYSjJNS0VzVitqRUtaSmI5dnBKYzV2aXVUaUxDTHlUaFVndmE5SlQzZndyMWZ3UkpCZjRiUGRta3EydzRBQT09XCIsXCJkZXZpY2VTaWduYXR1cmVcIjpcImVTR1BKZHRYc3FGYXNMVG94NFpRS0RFa2hnWlNFTjJod1hOOWQzSjcvVGdLSFBqOUtYcXpRSmRVN0tlVmtxdzVDUjIzbC9XdU0vRlhUVkNMLzM3ZkNRPT1cIn0sXCJzaWduYWxJZGVudGl0aWVzXCI6W3tcImlkZW50aWZpZXJcIjp7XCJuYW1lXCI6XCIyMzQ5MTY5NTgwMzAyOjlAcy53aGF0c2FwcC5uZXRcIixcImRldmljZUlkXCI6MH0sXCJpZGVudGlmaWVyS2V5XCI6e1widHlwZVwiOlwiQnVmZmVyXCIsXCJkYXRhXCI6XCJCWWlCL2lSR2hPVytPR1lud0xNd0Q5c3pDdXdSK2pPaWhMWUdaM3RUcUlJNVwifX1dLFwicGxhdGZvcm1cIjpcInNtYmFcIixcImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcFwiOjE3MDk4NDEyNzIsXCJteUFwcFN0YXRlS2V5SWRcIjpcIkFBQUFBTjYyXCJ9IiwKICAiYXBwLXN0YXRlLXN5bmMta2V5LUFBQUFBTjYyLmpzb24iOiAie1wia2V5RGF0YVwiOlwidlZZVFRERmcxSERGV1NKZ3poWVgvSXhiaG44b3JHNEtLMGtKY1dmaC8zdz1cIixcImZpbmdlcnByaW50XCI6e1wicmF3SWRcIjoxMTM2ODExODcyLFwiY3VycmVudEluZGV4XCI6MSxcImRldmljZUluZGV4ZXNcIjpbMCwxXX0sXCJ0aW1lc3RhbXBcIjpcIjBcIn0iCn0=" ;
 
 
 module.exports = {
